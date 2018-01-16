c9247b0da240d04ba78de9fa70ecc570ab73395b
o-julio <o.julio.machado@gmail.com>
2018-01-16 00:49:40 -0200

    Generating dynamic root Removing duplicate bundle import

--- app/index.js
+++ app/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
-import Home from 'screens/Home';
-import User from 'screens/User';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'assets/styles.css';
 
+const root = document.createElement('div');
+document.body.appendChild(root);
+
 ReactDOM.render(
   <Router history={browserHistory}>
-    <Route path="/" component={Home} />
-    <Route path="/:username" component={User} />
+    {/* <Route path="/" component={Home} /> */}
   </Router>,
-  document.getElementById('container')
+  root
 );
