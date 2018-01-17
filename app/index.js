65025bc83a763d9d4894cd3bf282936b685da69a
o-julio <o.julio.machado@gmail.com>
2018-01-16 23:14:57 -0200

    full react bro

--- app/index.js
+++ app/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'assets/styles.css';
+import User from 'screens/User'
+import Home from 'screens/Home'
 
 const root = document.createElement('div');
 document.body.appendChild(root);
 
 ReactDOM.render(
   <Router history={browserHistory}>
-    {/* <Route path="/" component={Home} /> */}
+    <Route path="/" component={Home} />
+    <Route path="/:username" component={User} />
   </Router>,
   root
 );
